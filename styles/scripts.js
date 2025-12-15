13a2e3c8b4f76a050726b5cc2baa24e9c292480d
YoteiKikuya <yotei_kikuya@sfu.ca>
2025-12-15 00:00:41 -0800

    Autoviscera + Homepage

--- styles/scripts.js
+++ styles/scripts.js
@@ -75,3 +75,16 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
+function openSection(evt, sectionName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("section");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-grey", "");
+  }
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " w3-grey";
+}
